71bf826dbb7c706f88572c90b794141290c72add
Alex <lileynuk@gmail.com>
2018-12-04 18:19:00 +0200

    Update maven project

---
+++ Lab3_Maven/src/main/java/sorters/BubbleSorter.java
@@ -0,0 +1,15 @@
+package sorters;
+
+/**
+ * <h1>BubbleSorter</h1>
+ * <p>This class is abstract.</p>
+ * <p>BubbleSorter extends AbstarctSorter.</p>
+ * <p>This class has abstract method - public int[] sort(int[] arr), which wasnt Overrided</p>
+ * @author Khomenko Olexandr
+ * @version 1.3
+ */
+public abstract class BubbleSorter extends AbstarctSorter {
+    /*@Override
+    public abstract int[] sort(int[] arr);*/
+
+}
